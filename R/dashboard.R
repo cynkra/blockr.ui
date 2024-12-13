0e83cd74a64aaaf2d364bbd8f607d939c2e9eb56
DivadNojnarg <dgranjon@ymail.com>
2024-12-13 16:45:54 +0100

    include registry + split modules scripts

---
+++ R/dashboard.R
@@ -0,0 +1,14 @@
+#' @rdname board
+#' @export
+dashboard_ui <- function(id) {
+  ns <- NS(id)
+  tagList()
+}
+
+#' @rdname board
+#' @export
+dashboard_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+  })
+}
---
+++ R/utils.R
@@ -0,0 +1,11 @@
+chr_ply <- function(x, fun, ..., length = 1L, use_names = FALSE) {
+  vapply(x, fun, character(length), ..., USE.NAMES = use_names)
+}
+
+lgl_ply <- function(x, fun, ..., length = 1L, use_names = FALSE) {
+  vapply(x, fun, logical(length), ..., USE.NAMES = use_names)
+}
+
+dropNulls <- function(x) {
+  x[!lgl_ply(x, is.null)]
+}
