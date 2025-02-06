72148e9968178200c4adfc05d198a53311e100f1
DivadNojnarg <dgranjon@ymail.com>
2025-02-06 22:40:59 +0100

    customize gen code ui

---
+++ R/plugin-code.R
@@ -0,0 +1,13 @@
+#' Code generation module
+#'
+#' Generate reproducible code from a board.
+#' @param board The initial `board` object
+#' @rdname gen_code
+#' @export
+gen_code_ui <- function(id, board) {
+  actionButton(
+    NS(id, "code_mod"),
+    "Code",
+    icon = icon("code")
+  )
+}
