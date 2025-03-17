4a3e41bd289447a84dcdc490283b70bcc77a70bc
DivadNojnarg <dgranjon@ymail.com>
2025-03-17 10:02:06 +0100

    use blockr.io

--- R/registry.R
+++ R/registry.R
@@ -1,16 +1,16 @@
 register_demo_blocks <- function() {
-  register_blocks(
-    c("new_readxpt_block", "new_readcsv_block"),
-    name = c(
-      "readxpt block",
-      "readcsv block"
-    ),
-    description = c(
-      "Upload and read xpt data",
-      "Upload and read csv data"
-    ),
-    category = c("parser", "parser"),
-    package = utils::packageName(),
-    overwrite = TRUE
-  )
+  #register_blocks(
+  #  c("new_readxpt_block", "new_readcsv_block"),
+  #  name = c(
+  #    "readxpt block",
+  #    "readcsv block"
+  #  ),
+  #  description = c(
+  #    "Upload and read xpt data",
+  #    "Upload and read csv data"
+  #  ),
+  #  category = c("parser", "parser"),
+  #  package = utils::packageName(),
+  #  overwrite = TRUE
+  #)
 }
