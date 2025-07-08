71ac30792ac26c8132ac099a8b303d03910cfb4d
DivadNojnarg <dgranjon@ymail.com>
2025-07-08 09:06:11 +0200

    update code plugin tests + block tests

--- R/plugin-code.R
+++ R/plugin-code.R
@@ -25,15 +25,15 @@ generate_code_server <- function(id, board, ...) {
     function(input, output, session) {
       dot_args <- list(...)
 
-      code <- reactive(
+      board_code <- reactive(
         export_code(lst_xtr_reval(board$blocks, "server", "expr"), board$board)
       )
+
+      output$code_out <- renderPrint(HTML(board_code()))
 
       observeEvent(
         req(dot_args$parent$display_code),
         {
-          output$code_out <- renderPrint(HTML(code()))
-
           id <- "code_out"
 
           showModal(
@@ -41,7 +41,7 @@ generate_code_server <- function(id, board, ...) {
               title = "Generated code",
               div(
                 class = "text-decoration-none position-relative",
-                if (nchar(code())) copy_to_clipboard(session, id),
+                if (nchar(board_code())) copy_to_clipboard(session, id),
                 verbatimTextOutput(session$ns(id))
               ),
               easyClose = TRUE,
@@ -49,7 +49,6 @@ generate_code_server <- function(id, board, ...) {
               size = "l"
             )
           )
-
           dot_args$parent$display_code <- FALSE
         }
       )
--- tests/testthat/test-block.R
+++ tests/testthat/test-block.R
@@ -6,43 +6,20 @@ test_that("get_block_registry works", {
 })
 
 
-my_board <- new_board(
-  blocks = new_dataset_block(),
-  class = "dash_board"
-)
+my_board <- new_dash_board(blocks = new_dataset_block())
 
 test_that("block ui works", {
   ui <- block_ui(
     "my_ui",
-    my_board
+    my_board,
+    board_blocks(my_board)
   )
-  expect_s3_class(ui, "shiny.tag.list")
+  expect_s3_class(ui, "shiny.tag")
   ui_query <- htmltools::tagQuery(ui)
   expect_true(grepl(board_block_ids(my_board), ui_query$allTags()$attribs$id))
 })
 
 test_that("remove_block_ui works", {
-  session <- as.environment(
-    list(
-      ns = identity,
-      sendRemoveUI = function(selector = selector, multiple = multiple) {
-        session$lastRemovedUI <- list(
-          selector = selector,
-          multiple = multiple
-        )
-      }
-    )
-  )
-
-  expect_error(remove_block_ui(character(), my_board, session = session))
-  remove_block_ui("my_ui", my_board, session = session)
-  expect_identical(session$lastRemovedUI$selector, "#my_ui_blocks > div")
-
-  remove_block_ui(
-    "my_ui",
-    my_board,
-    blocks = board_block_ids(my_board),
-    session = session
-  )
-  expect_true(grepl(board_block_ids(my_board), session$lastRemovedUI$selector))
+  session <- MockShinySession$new()
+  expect_null(remove_block_ui(character(), my_board, session = session))
 })
